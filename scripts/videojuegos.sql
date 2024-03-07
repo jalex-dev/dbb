6aef4b3e1873cd07ac4a10a5cabd9bf0ce5607c1
Joao <joaopriss@gmail.com>
2024-03-06 22:55:44 -0500

    Creacion de la tabla videojuegos

---
+++ scripts/videojuegos.sql
@@ -0,0 +1,8 @@
+CREATE TABLE videojuegos (
+  codigo INTEGER NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  descripcion VARCHAR(300),
+  valoracion INTEGER NOT NULL,
+  constraint videosjuegos_pk primary key(codigo)
+
+);
