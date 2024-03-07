9c2c4460087409a6b0620c10aa246993909f7611
Joao <joaopriss@gmail.com>
2024-03-06 22:31:20 -0500

    Creacion de la tabla productos

--- scripts/productos.sql
+++ scripts/productos.sql
@@ -1,7 +1,7 @@
 create table productos(
 	codigo int  not null,
 	nombre varchar(50)not null,
-	descripciom varchar(200) not null,
+	descripcion varchar(200),
 	precio money not null,
 	stock int not null,
 	constraint productos_pk primary key (codigo)
