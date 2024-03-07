a0623bbe191f290c87b69fe2b0d2533e97b5f18a
Joao <joaopriss@gmail.com>
2024-03-06 22:46:50 -0500

    Creacion de la tabla estudiantes

---
+++ scripts/estudiantes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE estudiantes (
+  cedula char(10),
+  nombre varchar(50) NOT NULL,
+  apellido varchar(50) NOT NULL,
+  fecha_nacimiento date NOT NULL,
+  email varchar(50) NOT NULL,
+  constraint estudiantes_pk primary key(cedula)
+);
